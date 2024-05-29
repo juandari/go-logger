6e4695b8f19a8195aa123c68f66165ab8bc9fe2c
juandari <juandaari70@gmail.com>
2024-05-29 21:26:57 +0700

    feat: add more logger

--- pocketlog/logger.go
+++ pocketlog/logger.go
@@ -1,21 +1,55 @@
 package pocketlog
 
+import (
+	"fmt"
+	"io"
+	"os"
+)
+
 type Logger struct {
 	threshold Level
+	output    io.Writer
 }
 
 // New returns you a logger, ready to log at the requried threshold.
-func New(threshold Level) *Logger {
+// The defualt output is Stdout
+func New(threshold Level, output io.Writer) *Logger {
 	return &Logger{
 		threshold: threshold,
+		output:    output,
 	}
 }
 
 func (l *Logger) Debugf(format string, args ...any) {
+	// making sure we can safely write to the output
+	if l.output == nil {
+		l.output = os.Stdout
+	}
+	if l.threshold <= LevelDebug {
+		l.logf(format, args...)
+	}
+
 }
 
 func (l *Logger) Infof(format string, args ...any) {
+	if l.output == nil {
+		l.output = os.Stdout
+	}
+	if l.threshold <= LevelInfo {
+		l.logf(format, args...)
+	}
+
 }
 
 func (l *Logger) Errorf(format string, args ...any) {
+	if l.output == nil {
+		l.output = os.Stdout
+	}
+	if l.threshold <= LevelError {
+		l.logf(format, args...)
+	}
+}
+
+func (l *Logger) logf(format string, args ...any) {
+	_, _ = fmt.Fprintf(l.output, format+"\n", args...)
 }
